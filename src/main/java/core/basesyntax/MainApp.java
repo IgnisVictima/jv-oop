e877e2b77a2cb75518f6bb5e76ee09a1a7b0cb56
Vitalii Smuraha <smuraga.v@gmail.com>
2026-04-10 09:06:58 +0200

    used auto-formating

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,20 +1,17 @@
 package core.basesyntax;
 
-public class MainApp{
-    public static void main(String[] args){
-        
+public class MainApp {
+    public static void main(String[] args) {
+
         Machine buldMachine = new Bulldozer();
         Machine truckMachine = new Truck();
         Machine excMachine = new Excavator();
 
+        Machine[] arrayOfMachines = new Machine[] { buldMachine, truckMachine, excMachine };
 
-        Machine[] arrayOfMachines = new Machine[] {buldMachine, truckMachine, excMachine};
-
-            for (Machine machine : arrayOfMachines){
-
-                machine.doWork();
-                machine.stopWork();
-            }
-
-    }  
+        for (Machine machine : arrayOfMachines) {
+            machine.doWork();
+            machine.stopWork();
+        }
+    }
 }
