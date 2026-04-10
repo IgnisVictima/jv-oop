5e3a3588e9c8517638fc49c3c6e97878ccc0ca0e
Vitalii Smuraha <smuraga.v@gmail.com>
2026-04-10 07:49:29 +0200

    add Main.Class Machine and his heirs

---
+++ src/main/java/core/basesyntax/Machine.java
@@ -0,0 +1,24 @@
+package core.basesyntax;
+
+public abstract class Machine {
+    
+    public abstract void doWork();
+
+    public abstract void stopWork();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,20 @@
 package core.basesyntax;
 
-public class MainApp {
+public class MainApp{
+    public static void main(String[] args){
+        
+        Machine buldMachine = new Bulldozer();
+        Machine truckMachine = new Truck();
+        Machine excMachine = new Excavator();
 
+
+        Machine[] arrayOfMachines = new Machine[] {buldMachine, truckMachine, excMachine};
+
+            for (Machine machine : arrayOfMachines){
+
+                machine.doWork();
+                machine.stopWork();
+            }
+
+    }  
 }
